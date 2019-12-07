4dbd7284b6957131e105347174f71c2a5f946ada
schwitin <wladimir.schwitin@gmail.com>
2019-12-07 11:40:43 +0100

    initial commit

---
+++ html/app.js
@@ -0,0 +1,32 @@
+// https://www.npmjs.com/package/node-static
+// https://nodejs.org/en/knowledge/HTTP/servers/how-to-serve-static-files/
+// https://stackoverflow.com/questions/44647778/how-to-run-shell-script-file-using-nodejs
+// https://nodejs.org/en/knowledge/HTTP/clients/how-to-access-query-string-parameters/
+// https://www.npmjs.com/package/shelljs
+
+const http = require('http');
+const url = require('url');
+const shell = require('shelljs')
+const static = require('node-static');
+
+var file = new(static.Server)();
+
+http.createServer(function (req, res) {
+  const queryObject = url.parse(req.url,true).query;
+
+
+  if (queryObject.barcode
+    && queryObject.name
+    && queryObject.amount
+  ){
+    shell.exec('./generate-label.sh ' + queryObject.barcode + ' '  + queryObject.name + ' ' + queryObject.amount)
+   }
+
+   req.addListener('end', function () {
+           file.serve(req, res);
+   }).resume();
+
+
+}).listen(8080);
+
+
---
+++ html/generate-label.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# https://phantomjs.org/examples
+# https://pypi.org/project/brother-ql/
+
+cp template.html index.html
+echo $1 $2 $3
+sed -i  "s/BARCODE/$1/g" index.html
+sed -i  "s/NAME/$2/g" index.html
+sed -i  "s/AMOUNT/$3/g" index.html
+
+phantomjs rasterize.js file://`pwd`/index.html index.png 500px*180px
+
