49fe4340fe17ee5d74044412a9c9b393a4bf72e9
schwitin <wladimir.schwitin@gmail.com>
2019-12-13 22:49:02 +0100

    Label überarbeitet

--- html/generate-label.sh
+++ html/generate-label.sh
@@ -12,4 +12,4 @@ sed -i  "s/NAME/$2/g" index.html
 sed -i  "s/AMOUNT/$3/g" index.html
 sed -i  "s/ME/$4/g" index.html
 
-phantomjs rasterize.js file://`pwd`/index.html index.png 696px*300px
+phantomjs rasterize.js file://`pwd`/index.html index.png 696px*280px
