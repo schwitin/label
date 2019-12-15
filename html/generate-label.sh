0dd349a42583a1aa170e12c56e400c52b20f9f54
schwitin <wladimir.schwitin@gmail.com>
2019-12-15 16:56:52 +0100

    .gitignore

--- html/generate-label.sh
+++ html/generate-label.sh
@@ -9,11 +9,10 @@ export QT_QPA_PLATFORM=offscreen
 export BROTHER_QL_MODEL=QL-820NWB
 export BROTHER_QL_PRINTER=tcp://BRW105BAD1C7BB8.local
 
-cp template.html index.html
 
 # Zu lange String abschneiden
 BARCODE=$(echo $1 | colrm 17)
-NAME=$(echo $2 | colrm 28)
+NAME=$(echo $2 | colrm 27)
 AMOUNT=$(echo $3 | colrm 6)
 ME=$(echo $4 | colrm 4)
 ETIKETTEN=$(echo $5 | colrm 3)
@@ -32,17 +31,14 @@ echo "ETIKETTEN=$ETIKETTEN"
 # ETIKETTEN=$5
 
 # Platzhalter in HTML eresetzen
-sed -i  "s/BARCODE/$BARCODE/g" index.html
-sed -i  "s/NAME/$NAME/g" index.html
-sed -i  "s/AMOUNT/$AMOUNT/g" index.html
-sed -i  "s/ME/$ME/g" index.html
+sed -e  "s/BARCODE/$BARCODE/g" -e "s/NAME/$NAME/g" -e "s/AMOUNT/$AMOUNT/g" -e "s/ME/$ME/g" template.html > index.html
 
 # html2png
-phantomjs rasterize.js file://`pwd`/index.html index.png 696px*280px
+phantomjs rasterize.js file://`pwd`/index.html index.png  696px*271px
 
 # print
 for (( i = 0; i < $ETIKETTEN; i++ )) 
 do
-  # echo $i
+  #echo $i
   /home/stalker/.local/bin/brother_ql print -l62 --red index.png
 done
